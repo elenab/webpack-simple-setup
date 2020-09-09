3d05471c6176c1b2b800ec1beb0664def1b6d8b5
Elena Busila <elena.busila@gmail.com>
2020-09-09 09:33:37 -0400

    Setting up Typescript using ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: "./src/index.ts",
+    resolve:{
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    module:{
+        rules:[{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
+        }]
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './src/index.html',
